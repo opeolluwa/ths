d966cb4f05ae83a71edddc9c9fbc987590da7690
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-06-24 11:41:32 +0100

    CHORE -> update

--- src/inquirer.rs
+++ src/inquirer.rs
@@ -1,16 +1,32 @@
 //utility class for parsing command line arguments
 extern crate dialoguer;
-use dialoguer::{theme::CustomPromptCharacterTheme, Confirmation, Input, Select as DialougerSelect};
+extern crate console;
 
-// pub struct Inquirer {}
+use console::Style;
+use dialoguer::{
+    theme::ColorfulTheme, Confirmation, Input, Select as DialougerSelect,
+};
+
 pub struct Prompt {}
+// #[derive(Clone, Copy)]
 pub struct Select {}
 pub struct Confirm {}
+
+
+//custom theme 
+
 
 //inquirer::Prompt used to prompt the user for input
 impl Prompt {
     pub fn new(query: String) -> String {
-        let theme = CustomPromptCharacterTheme::new('>');
+        // let theme = CustomPromptCharacterTheme::new('>');
+        let theme = ColorfulTheme {
+            values_style: Style::new().yellow().bright(),
+            indicator_style: Style::new().yellow().bold(),
+            yes_style: Style::new().yellow().bright(),
+            no_style: Style::new().yellow().bright(),
+            ..ColorfulTheme::default()
+        };
         let answer: String = Input::with_theme(&theme)
             .with_prompt(&query)
             .interact()
@@ -27,16 +43,26 @@ impl Confirm {
     }
 }
 
+//Inquirer :: Select use to select option from command line
+
 impl Select {
     pub fn new(query: String, options: Vec<String>) -> String {
-        let theme = CustomPromptCharacterTheme::new('>');
-        let answer = DialougerSelect::with_theme(&theme)
+        let theme = ColorfulTheme {
+            values_style: Style::new().yellow().bright(),
+            indicator_style: Style::new().yellow().bold(),
+            yes_style: Style::new().yellow().bright(),
+            no_style: Style::new().yellow().bright(),
+            ..ColorfulTheme::default()
+        };
+        let index = DialougerSelect::with_theme(&theme)
             .with_prompt(&query)
             .default(0)
             .items(&options[..])
             .interact()
             .unwrap();
-        answer.to_string()
+
+         options[index].to_string()
+        
     }
 }
 
